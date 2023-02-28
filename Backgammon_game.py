5d71021c6255e36008381c33a5c9103a1ae41a27
Artem Efimov <artsefimov@gmail.com>
2023-02-28 21:51:55 +0300

    working

--- Backgammon_game.py
+++ Backgammon_game.py
@@ -4,12 +4,10 @@ class MyList(list):
 
     def __getitem__(self, item):
         if isinstance(item, slice):
-            print(item.indices(len(self)))
             start, stop, step = item.indices(len(self))
             modify_slice = slice((start - 1 if start not in (len(self) - 1, 0) else None),
                                  (stop - 1 if stop not in (len(self), -1) else None),
                                  step)
-            print(modify_slice)
             return super().__getitem__(modify_slice)
         if item - 1 in range(len(self)):
             return super().__getitem__(item - 1)
@@ -21,42 +19,70 @@ class MyList(list):
                                  (stop - 1 if stop not in (len(self), -1) else None),
                                  step)
             super().__setitem__(modify_slice, value)
-        if key - 1 in range(len(self)):
-            super(MyList, self).__setitem__(key - 1, value)
+        else:
+            if key - 1 in range(len(self)):
+                super(MyList, self).__setitem__(key - 1, value)
 
 
-ml = MyList([i for i in range(1, 11)])
-ml[1:5] = [2, 2, 2, 2]
-print(ml)
+class FieldStructure:
+    def __init__(self, data, *args, **kwargs):
+        self.data = data
+        self.next_element = None
+        self.previous_element = None
+
+    def connect_elements(self, next_element=None, previous_element=None):
+        if next_element:
+            self.next_element = next_element
+        if previous_element:
+            self.previous_element = previous_element
+
+
+class Checker:
+    def __init__(self, color):
+        self.color = color.lower()
+        self.on_the_head = True
+
+    def __repr__(self):
+        return self.color
+
+    def __str__(self):
+        return 'b' if self.color == 'black' else 'w'
 
 
 class Field:
     def __init__(self):
-        self.white_home = MyList([0] * 15)
-        self.black_home = MyList([0] * 15)
-        self.white_yard = MyList([0] * 15)
-        self.black_yard = MyList([0] * 15)
+        self.white_home = FieldStructure(MyList([0] * 6))
+        self.black_home = FieldStructure(MyList([0] * 6))
+        self.white_yard = FieldStructure(MyList([0] * 6))
+        self.black_yard = FieldStructure(MyList([0] * 6))
 
-        self.field = MyList()
+        self.white_start = self.white_home
+        self.black_start = self.black_home
 
-    def init_field(self):
-        self.white_home[1] = 'X' * 15
-        self.black_home[1] = 'X' * 15
-        self.field.append(self.white_home)
-        self.field.append(self.black_home)
-        self.field.append(self.white_yard)
-        self.field.append(self.black_yard)
+    def init_field_and_create_field_structure(self):
+        self.white_home.data[1] = MyList([Checker('white') for _ in range(15)])
+        self.black_home.data[1] = MyList([Checker('black') for _ in range(15)])
+        self.white_home.connect_elements(next_element=self.white_yard)
+        self.white_yard.connect_elements(previous_element=self.white_home, next_element=self.black_home)
+        self.black_home.connect_elements(next_element=self.black_yard)
+        self.black_yard.connect_elements(previous_element=self.black_home, next_element=self.white_home)
 
-#     def show_field(self):
-#         for row in self.field:
-#             print(row[::-1])
-#             # row = row[::-1]
-#             # for element in row:
-#             #     print(element)
-#
-#
-# f = Field()
-#
-# f.init_field()
-#
-# f.show_field()
+    def show_field(self):
+        start = self.white_start
+        for _ in range(4):
+            tmp_data = start.data
+            for element in tmp_data:
+                if type(element) == MyList:
+                    print(*element)
+                else:
+                    print(element)
+            print()
+
+            start = start.next_element
+
+
+f = Field()
+
+f.init_field_and_create_field_structure()
+
+f.show_field()
