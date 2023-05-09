4a6943300f96ac721a63c14a2857016f8c681123
Artem <artsefimov@gmail.com>
2023-05-09 23:46:49 +0300

    thinking

--- Backgammon_game/backgammon_game.py
+++ Backgammon_game/backgammon_game.py
@@ -169,8 +169,8 @@ class Game:
 
         if current_phase == 2:
             return {
-                1: 12, 2: 1, 3: 2, 4: 3, 5: 4, 6: 5,
-                7: 6, 8: 11, 9: 10, 10: 9, 11: 8, 12: 7,
+                1: 12, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6,
+                7: 7, 8: 12, 9: 11, 10: 10, 11: 9, 12: 8,
                 13: -5, 14: -4, 15: -3, 16: -2, 17: -1, 18: 0,
                 19: 0, 20: 0, 21: 0, 22: 0, 23: 0, 24: 0, 25: 0
             }
@@ -205,9 +205,9 @@ class Game:
 
         if current_phase == 2:
             return {
-                2: 1, 3: 2, 4: 3, 5: 4, 6: 5, 7: 6,
-                8: 7, 9: 8, 10: 9, 11: 10, 12: 11,
-                13: 17, 14: 16, 15: 15, 16: 14, 17: 13, 18: 12,
+                2: 12, 3: 11, 4: 10, 5: 9, 6: 8, 7: 7,
+                8: 8, 9: 9, 10: 10, 11: 11, 12: 12,
+                13: 23, 14: 22, 15: 21, 16: 20, 17: 19, 18: 18,
                 19: 0, 20: 0, 21: 0, 22: 0, 23: 0, 24: 0
             }
 
@@ -354,27 +354,27 @@ class Game:
                 if isinstance(old_position, MyStack):
                     if old_position.count > 1:
 
-                        value = old_position.count * self.get_ratio_from(old_position.count)
 
                         # if old_position is self.black_head or old_position is self.white_head:
                         #     value += old_position.count
 
-                        count += value
+                        count += old_position.count
 
                     elif old_position.count == 1:
-                        count -= 7 # в зависимости от четверти и фазы игры (
+                        count -= 0 # в зависимости от четверти и фазы игры (
                     # например надо приоритетнее снять в 3-й четверти во второй фазе)
 
                 if isinstance(new_position, MyStack):
-                    count -= new_position.count * self.get_ratio_to(new_position.count)
+                    count -= new_position.count
 
                 else:
-                    count += 5  # в зависимости от четверти и фазы игры (
+                    count += 0  # в зависимости от четверти и фазы игры (
                     # например надо приоритетнее снять в 3-й четверти во второй фазе)
 
                 counts[checker_value] = count
 
         if counts:
+
             sorted_counts = sorted(counts, key=lambda c: counts[c], reverse=True)
             checker = sorted_counts[0]
             count = counts[checker]
